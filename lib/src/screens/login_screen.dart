74f7a2f250885fa34d6baf5697533fff46afe4a4
steadysamwise4 <davenportsam44@gmail.com>
2023-06-07 07:55:45 -0400

    login screen set up with email input(unconnected to state)

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            emailField(),
+            // passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+      keyboardType: TextInputType.emailAddress,
+    );
+  }
+
+  // Widget passwordField() {}
+
+  // Widget submitButton() {}
+}
